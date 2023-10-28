b7dd51389b77d3128674c3ccb0f5adf5b46a9be2
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-10-28 17:28:32 +0300

    improved the DOM update
    Now by changing the servings only the necessary content is updated.

--- src/js/controller.js
+++ src/js/controller.js
@@ -48,7 +48,8 @@ const controlPagination = function (goToPage) {
 
 const controlServings = function (newServings) {
   model.updateServings(newServings);
-  recipeView.render(model.state.recipe);
+  //recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
