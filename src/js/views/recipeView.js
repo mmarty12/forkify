086f1572abbf3c8d935f86edf1b0c276b7c02c04
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-10-30 13:46:41 +0200

    added external library "fracty" for more accurate quantity display
    + fixed the bug by adding another condition statement.

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,3 +1,4 @@
+import fracty from 'fracty';
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 
@@ -98,7 +99,9 @@ class RecipeView extends View {
           <svg class="recipe__icon">
             <use href="${icons}#icon-check"></use>
           </svg>
-          <div class="recipe__quantity">${ing.quantity}</div>
+          <div class="recipe__quantity">${
+            ing.quantity ? fracty(ing.quantity).toString() : ''
+          }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
             ${ing.description}
